aa82946faa2c890af682290d932eb4753f1c3146
Gustavo Marengo <gustavo.marengo@venturus.org.br>
2022-04-22 19:26:14 -0300

    app_angular_receitas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipe-app';
+  i = 0;
+
+  somar(){
+    console.log(this.i = this.i+1);
+  }
 }
---
+++ src/app/shopping/shopping-list/shopping-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredients.modal';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Maçã', 5),
+    new Ingredient('Carne', 10)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
